5f42f170f0579ddc21b4aa999dba48b738f3926b
canhlinh <canhlinh.lienson@gmail.com>
2018-12-03 18:01:50 +0700

    Adds more pad types

--- binary_pack_test.go
+++ binary_pack_test.go
@@ -1,15 +1,16 @@
-package binary_pack
+package binarypack
 
 import (
-	"testing"
+	"math"
 	"reflect"
+	"testing"
 )
 
 func TestBinaryPack_CalcSize(t *testing.T) {
 	cases := []struct {
-		in []string
+		in   []string
 		want int
-		e bool
+		e    bool
 	}{
 		{[]string{}, 0, false},
 		{[]string{"I", "I", "I", "4s"}, 16, false},
@@ -35,28 +36,23 @@ func TestBinaryPack_CalcSize(t *testing.T) {
 
 func TestBinaryPack_Pack(t *testing.T) {
 	cases := []struct {
-		f []string
-		a []interface{}
+		f    []string
+		a    []interface{}
 		want []byte
-		e bool
+		e    bool
 	}{
 		{[]string{"?", "?"}, []interface{}{true, false}, []byte{1, 0}, false},
-		{[]string{"h", "h", "h"}, []interface{}{0, 5, -5},
-			[]byte{0, 0, 5, 0, 251, 255}, false},
-		{[]string{"H", "H", "H"}, []interface{}{0, 5, 2300},
-			[]byte{0, 0, 5, 0, 252, 8}, false},
-		{[]string{"i", "i", "i"}, []interface{}{0, 5, -5},
-			[]byte{0, 0, 0, 0, 5, 0, 0, 0, 251, 255, 255, 255}, false},
-		{[]string{"I", "I", "I"}, []interface{}{0, 5, 2300},
-			[]byte{0, 0, 0, 0, 5, 0, 0, 0, 252, 8, 0, 0}, false},
-		{[]string{"f", "f", "f"}, []interface{}{float32(0.0), float32(5.3), float32(-5.3)},
-			[]byte{0, 0, 0, 0, 154, 153, 169, 64, 154, 153, 169, 192}, false},
-		{[]string{"d", "d", "d"}, []interface{}{0.0, 5.3, -5.3},
-			[]byte{0, 0, 0, 0, 0, 0, 0, 0, 51, 51, 51, 51, 51, 51, 21, 64, 51, 51, 51, 51, 51, 51, 21, 192}, false},
-		{[]string{"1s", "2s", "10s"}, []interface{}{"a", "bb", "1234567890"},
-			[]byte{97, 98, 98, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48}, false},
-		{[]string{"I", "I", "I", "4s"}, []interface{}{1, 2, 4, "DUMP"},
-			[]byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80}, false},
+		{[]string{"B", "B", "B"}, []interface{}{0, 5, 255}, []byte{0, 5, 255}, false},
+		{[]string{"b", "b", "b"}, []interface{}{0, -128, 127}, []byte{0, 128, 127}, false},
+		{[]string{"h", "h", "h"}, []interface{}{0, 5, -5}, []byte{0, 0, 5, 0, 251, 255}, false},
+		{[]string{"H", "H", "H"}, []interface{}{0, 5, math.MaxInt16}, []byte{0, 0, 5, 0, 255, 127}, false},
+		{[]string{"i", "i", "i"}, []interface{}{0, 5, -5}, []byte{0, 0, 0, 0, 5, 0, 0, 0, 251, 255, 255, 255}, false},
+		{[]string{"I", "I", "I"}, []interface{}{0, 5, math.MaxInt32}, []byte{0, 0, 0, 0, 5, 0, 0, 0, 255, 255, 255, 127}, false},
+		{[]string{"q", "Q"}, []interface{}{math.MinInt64, math.MaxInt64}, []byte{0, 0, 0, 0, 0, 0, 0, 128, 255, 255, 255, 255, 255, 255, 255, 127}, false},
+		{[]string{"f", "f", "f"}, []interface{}{float32(0.0), float32(5.3), float32(-5.3)}, []byte{0, 0, 0, 0, 154, 153, 169, 64, 154, 153, 169, 192}, false},
+		{[]string{"d", "d", "d"}, []interface{}{0.0, 5.3, -5.3}, []byte{0, 0, 0, 0, 0, 0, 0, 0, 51, 51, 51, 51, 51, 51, 21, 64, 51, 51, 51, 51, 51, 51, 21, 192}, false},
+		{[]string{"1s", "2s", "10s"}, []interface{}{"a", "bb", "1234567890"}, []byte{97, 98, 98, 49, 50, 51, 52, 53, 54, 55, 56, 57, 48}, false},
+		{[]string{"I", "I", "I", "4s"}, []interface{}{1, 2, 4, "DUMP"}, []byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80}, false},
 		// Wrong format length
 		{[]string{"I", "I", "I", "4s"}, []interface{}{1, 4, "DUMP"}, nil, true},
 		// Wrong format token
@@ -86,12 +82,14 @@ func TestBinaryPack_Pack(t *testing.T) {
 
 func TestBinaryPack_UnPack(t *testing.T) {
 	cases := []struct {
-		f []string
-		a []byte
+		f    []string
+		a    []byte
 		want []interface{}
-		e bool
+		e    bool
 	}{
 		{[]string{"?", "?"}, []byte{1, 0}, []interface{}{true, false}, false},
+		{[]string{"B", "B", "B"}, []byte{0, 5, 255}, []interface{}{0, 5, 255}, false},
+		{[]string{"b", "b", "b"}, []byte{0, 128, 127}, []interface{}{0, -128, 127}, false},
 		{[]string{"h", "h", "h"}, []byte{0, 0, 5, 0, 251, 255},
 			[]interface{}{0, 5, -5}, false},
 		{[]string{"H", "H", "H"}, []byte{0, 0, 5, 0, 252, 8},
@@ -135,11 +133,11 @@ func TestBinaryPack_UnPack(t *testing.T) {
 
 func TestBinaryPackPartialRead(t *testing.T) {
 	cases := []struct {
-		f []string
-		a []byte
-		i int // Position of expected value
+		f    []string
+		a    []byte
+		i    int // Position of expected value
 		want interface{}
-		e bool
+		e    bool
 	}{
 		{[]string{"I", "I", "I"}, // []interface{}{1, 2, 4, "DUMP"} <- encoded collection has 4 values
 			[]byte{1, 0, 0, 0, 2, 0, 0, 0, 4, 0, 0, 0, 68, 85, 77, 80}, 2, 4, false},
